9d657d88b3a627489ba4a305e8be2008ad6ee443
darkot1 <sebastian.serna.1998@gmail.com>
2025-08-26 18:46:04 -0500

    fix: userRepository findAll flux

--- auth-service/domain/model/src/main/java/com/pragma/model/user/gateways/UserRepository.java
+++ auth-service/domain/model/src/main/java/com/pragma/model/user/gateways/UserRepository.java
@@ -1,15 +1,16 @@
 package com.pragma.model.user.gateways;
 
 import com.pragma.model.user.User;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository {
 
     Mono<User> create(User user);
     Mono<User> findByEmail(String email);
-    Mono<User> findAll();
+    Flux<User> findAll();
     Mono<User> update(User user);
-    Mono<User> delete(String email);
+    Mono<Void> delete(String email);
 
 
 }
